a2f9da25e2c77b0c31ec8bba448403ff231f0829
Christophe Thibaut <cthibaut@octo.com>
2021-03-13 10:16:07 +0100

    a room has its temperature changing at each evolution

--- src/Room.hs
+++ src/Room.hs
@@ -1,16 +1,27 @@
-module Room (Room, cursorPosition, newRoom, setCursorPosition, temperature)
+module Room (Room, cursorPosition, newRoom, setCursorPosition, temperature, evolve)
     where
 
 type Temperature = Double
 type CursorPosition = Int
 data Room = Room { 
-    temperature :: Temperature,
+    temperatures :: [Temperature],
     cursorPosition :: CursorPosition }
     deriving (Eq, Show)
 
 
 newRoom :: Room
-newRoom = Room 15.0 100
+newRoom = Room [15.0, 15.0, 15.0, 15.0, 15.0] 100
 
 setCursorPosition :: Room -> CursorPosition -> Room
 setCursorPosition room curPos = room { cursorPosition = curPos }
+
+temperature :: Room -> Temperature
+temperature room = head (temperatures room)
+
+evolve :: Room -> Room
+evolve room = room { temperatures = temperatures' }
+    where
+        temperatures' = (temperature room + delta) : temperatures room
+        delta = (fromIntegral (cursorPosition room) 
+                / 10.0 + 2.0 - (temperatures room) !! 4) 
+                / 3.0
--- test/RoomSpec.hs
+++ test/RoomSpec.hs
@@ -4,6 +4,10 @@ module RoomSpec
 import Test.Hspec
 import Room
 
+evolutions :: Int -> Room -> Room
+evolutions 0 room = room
+evolutions n room = evolutions (pred n) (evolve room)
+
 spec :: SpecWith ()
 spec = do
     describe "A room" $ do
@@ -15,5 +19,12 @@ spec = do
         it "can have its cursor position changed" $ do
             let room = setCursorPosition newRoom 20
             cursorPosition room `shouldBe` 20
-                
 
+        it "has its temperature evolving at each evolution" $ do
+            temperature newRoom `shouldBe` 15.0
+            temperature (evolutions 1 newRoom) `shouldBe` 14.0
+            temperature (evolutions 2 newRoom) `shouldBe` 13.0
+            temperature (evolutions 3 newRoom) `shouldBe` 12.0
+            temperature (evolutions 4 newRoom) `shouldBe` 11.0
+            temperature (evolutions 5 newRoom) `shouldBe` 10.0
+            temperature (evolutions 6 newRoom) `shouldBe` 9.333333333333334
