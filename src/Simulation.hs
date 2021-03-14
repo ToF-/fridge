10dbc67fc3732cb28e75e84a5673042ff74b0c9b
Christophe Thibaut <cthibaut@octo.com>
2021-03-14 17:03:49 +0100

    refactoring

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -3,6 +3,7 @@ module Simulation (Simulation, addSituation, changeSituation, getState, newSimul
 
 import Room
 import Situation
+import Data.Maybe (fromJust)
 
 type Name = String
 data Simulation = Simulation [(Name, Situation)]
@@ -12,9 +13,8 @@ newSimulation :: Simulation
 newSimulation = Simulation []
 
 getState :: Name -> Simulation -> Either String (State, Temperature, CursorPosition)
-getState name (Simulation ss) = case lookup name ss of
-                      Nothing -> Left ("no situation exists with name:" ++ name)
-                      Just sit -> Right (state sit, temperature (room sit), cursorPosition (room sit))
+getState name s = return s >>= checkName name 
+    >>= (\(Simulation ss) -> let sit = fromJust (lookup name ss) in Right (state sit, temperature (room sit), cursorPosition (room sit)))
 
 addSituation :: Name -> Simulation -> Either String Simulation
 addSituation name (Simulation ss) = case lookup name ss of
@@ -22,18 +22,21 @@ addSituation name (Simulation ss) = case lookup name ss of
                                       Just _ -> Left ("a situation already exists with name:" ++ name)
 
 startSituation :: Name -> Simulation -> Either String Simulation
-startSituation name (Simulation ss) = case lookup name ss of
-                                        Nothing -> Left ("no situation exists with name:" ++ name)
-                                        Just sit -> Right (Simulation (update name (start sit) ss))
+startSituation name s = return s >>= checkName name 
+    >>= (\(Simulation ss) -> Right (Simulation (update name start ss)))
 
-update :: Name -> Situation -> [(Name, Situation)] -> [(Name, Situation)]
+update :: Name -> (Situation -> Situation) -> [(Name, Situation)] -> [(Name, Situation)]
 update _ _ [] = []
-update target new ((name,sit):ss) | name == target = ((name, new) : ss) 
-                                  | otherwise      = (name,sit): update target new ss
+update target f ((name,sit):ss) | name == target = (name, f sit) : ss
+                                | otherwise      = (name,sit): update target f ss
+
+checkName :: Name -> Simulation -> Either String Simulation
+checkName name (Simulation ss) = case lookup name ss of
+                                   Nothing -> Left ("no situation exists with name:" ++ name)
+                                   Just _ -> Right (Simulation ss)
 
 changeSituation :: Name -> CursorPosition -> Simulation -> Either String Simulation
-changeSituation name pos (Simulation ss) = case lookup name ss of
-                                    Nothing -> Left ("no situation exists with name:" ++ name)
-                                    Just sit -> case state sit of
-                                                  Started -> Right (Simulation (update name (change pos sit) ss))
-                                                  Halted -> Left ("situation for "++name++" is not started")
+changeSituation name pos s = return s >>= checkName name
+    >>= (\(Simulation ss) -> let sit = fromJust (lookup name ss) in case state sit of
+                                                                      Started -> Right (Simulation (update name (change pos) ss))
+                                                                      Halted -> Left ("situation for " ++ name ++ " is not started"))
