78730e485004d01a0606244b627e9ba083b7da1f
Christophe Thibaut <cthibaut@octo.com>
2021-03-15 22:27:41 +0100

    web application can post changes

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 
 module Main where
 
+import           Situation
 import           Simulation
 import           Web.Spock
 import           Web.Spock.Config
@@ -16,6 +17,15 @@ import           GHC.Generics
 import           Control.Monad.Trans (liftIO)
 
 data AppState = AppState (IORef (Either String Simulation))
+
+data Command = Start | Halt | Reset | Change Int
+    deriving (Eq, Show, Generic)
+
+instance ToJSON Command
+instance FromJSON Command
+
+data Action = Action Command Name
+    deriving (Eq, Show, Generic)
 
 type Api = SpockM () () AppState ()
 type ApiAction a = SpockAction () () AppState a
@@ -32,10 +42,27 @@ app = do
         (AppState ref) <- Web.Spock.getState
         simulation <- liftIO $ readIORef ref
         json $ simulation
+
     get ("situation" <//> var) $ \name -> do
         (AppState ref) <- Web.Spock.getState
         simulation <- liftIO $ readIORef ref
         json $ simulation >>= Simulation.getState name
+
+    post ("action" <//> var) $ \name -> do
+        command <- jsonBody' :: ApiAction Command
+        let f = case command of
+                  Start -> apply start
+                  Halt -> apply halt
+                  Reset -> apply reset
+                  (Change n) -> (changeSituation n)
+        (AppState ref) <- Web.Spock.getState
+        simulation <- liftIO $ readIORef ref
+        let simulation' = simulation >>= f name
+        case simulation' of
+            Left _ -> json $ simulation'
+            Right sim -> do
+                result <- liftIO $ atomicModifyIORef' ref $ const (simulation', simulation')
+                json $ result >>= Simulation.getState name
 
     post "situation" $ do
         name <- jsonBody' :: ApiAction Name
