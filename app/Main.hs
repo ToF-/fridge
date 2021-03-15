3e4ad3d6cb108666495af7cc99982f4fdb77f351
Christophe Thibaut <cthibaut@octo.com>
2021-03-15 15:53:51 +0100

    simple main app web server on localhost:8080 responding to simulation and state/<name>

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Simulation
+import           Web.Spock
+import           Web.Spock.Config
+import           Data.IORef
+
+import           Data.Aeson       hiding (json)
+import           Data.Monoid      ((<>))
+import           Data.Text        (Text, pack)
+import           GHC.Generics
+
+import           Control.Monad.Trans (liftIO)
+
+data AppState = AppState (IORef (Either String Simulation))
+
+type Api = SpockM () () AppState ()
 
 main :: IO ()
-main = someFunc
+main = do
+    ref <- newIORef (return newSimulation >>= addSituation "ToF")
+    spockCfg <- defaultSpockCfg () PCNoDatabase (AppState ref)
+    runSpock 8080 (spock spockCfg app)
+
+app :: Api
+app = do
+    get "simulation" $ do
+        (AppState ref) <- Web.Spock.getState
+        simulation <- liftIO $ readIORef ref
+        json $ simulation
+    get ("state" <//> var) $ \name -> do
+        (AppState ref) <- Web.Spock.getState
+        simulation <- liftIO $ readIORef ref
+        json $ simulation >>= Simulation.getState name
