d2a09d72dbc06cd3b85a23e9ce3840b96c2fe97b
Christophe Thibaut <cthibaut@octo.com>
2021-03-20 10:59:10 +0100

    refactoring

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -13,7 +13,6 @@ module Simulation ( Simulation (..)
 import Control.FromSum (maybeToEither)
 import Data.Aeson
 import Data.Map as M
-import Data.Maybe (fromJust)
 import GHC.Generics
 import Room
 import Situation
@@ -36,28 +35,33 @@ view name simulation = Situation.view <$> situation
         message   = "no situation exists with name:" ++ name
 
 add :: Name -> Simulation -> Either String Simulation
-add name simulation = 
+add name simulation =
     case (name ?? simulation) of
       Nothing -> pure (name !> simulation)
       Just _  -> Left ("a situation already exists with name:" ++ name)
 
 (??) :: Name -> Simulation -> Maybe Situation
-name ?? simulation = M.lookup name (situations simulation) 
+name ?? simulation = M.lookup name (situations simulation)
 
 (!>) :: Name -> Simulation -> Simulation
 name !> simulation = Simulation situations'
     where situations' = M.insert name Situation.new (situations simulation)
 
+nonExisting :: Name -> Either String a
+nonExisting name = Left ("no situation exists with name:" ++ name)
+
 checkName :: Name -> Simulation -> Either String Simulation
 checkName name (Simulation ss) = case M.lookup name ss of
-                                   Nothing -> Left ("no situation exists with name:" ++ name)
+                                   Nothing -> nonExisting name
                                    Just _ -> Right (Simulation ss)
 
 change :: CursorPosition -> Name -> Simulation -> Either String Simulation
-change pos name s = return s >>= checkName name
-    >>= (\(Simulation ss) -> let sit = fromJust (M.lookup name ss) in case state sit of
-                                                                      Started -> Right (Simulation (M.adjust (Situation.change pos) name ss))
-                                                                      Halted -> Left ("situation for " ++ name ++ " is not started"))
+change pos name simulation =
+    case (name ?? simulation) of
+      Nothing -> nonExisting name
+      Just situation -> case state situation of
+                          Started -> Right $ Simulation { situations = M.adjust (Situation.change pos) name (situations simulation) }
+                          Halted -> Left ("situation for " ++ name ++ " is not started")
 
 apply :: (Situation -> Situation) -> Name -> Simulation -> Either String Simulation
 apply f name s = return s >>= checkName name
