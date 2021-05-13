74db0c1d37304bd82435f39f028569739cac05f5
Christophe Thibaut <cthibaut@octo.com>
2021-05-13 10:30:50 +0200

    restart, version 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,1 @@
-module Main where
-
-import Server                 (app)
-import System.Environment  (getArgs)
-import Web.Spock           (runSpock)
-
-main :: IO ()
-main = do
-    args <- getArgs
-    let refresh = case args of
-              [] -> 60
-              (s:_) -> read s
-    putStrLn $ "refresh every " ++ show refresh ++ " seconds"
-    runSpock 8080 (app refresh)
+main = putStrLn "nothing to see here"
