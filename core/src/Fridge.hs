61b7eb1687d93cee615a1d3238b0595f4fe2f8d9
Christophe Thibaut <cthibaut@octo.com>
2021-05-24 18:01:07 +0200

    moved core to fridge-core

--- core/src/Fridge.hs
+++
@@ -1,3 +0,0 @@
-module Fridge
-    where
-
--- fridge-graph/app/Main.hs
+++ fridge-graph/app/Main.hs
@@ -1,6 +1,24 @@
-module Main where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+module Main where
+import Room
+import Simulation
+import History
+import GraphHtml
+import Data.Aeson
+import Text.Blaze.Html.Renderer.String
+import Data.ByteString.Lazy.Char8 as BS
+
+instance FromJSON Room
+instance FromJSON RoomState
+instance FromJSON History
+instance FromJSON Simulation
 
 main :: IO ()
-main = someFunc
+main = do
+    content <- BS.readFile "simulation.json"
+    let found = decode content
+    case found of
+      Nothing -> Prelude.putStrLn "uh oh"
+      Just sim -> Prelude.putStrLn $ renderHtml $ graphHtml sim
